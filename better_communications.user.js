2b16f1d159dd4803e84a51a49840f7a90c121daf
Akulen <Akulen@users.noreply.github.com>
2015-10-27 00:04:52 +0100

    Init

---
+++ better_communications.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Asylamba Better Communications
+// @namespace   Akaito
+// @include     http://game.asylamba.com/s8/map
+// @updateURL   ???
+// @version     1
+// @grant       none
+// ==/UserScript==
