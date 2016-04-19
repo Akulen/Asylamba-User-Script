cd03561ba15e62cb19c894b51fd2049852e87933
Ayaash <hugo.ayats@hotmail.fr>
2016-04-19 15:34:43 +0200

    Modification s10

--- better fleet_user_script_Beta4.user.js
+++ better fleet_user_script_Beta4.user.js
@@ -7,6 +7,7 @@
 // @match        http://game.asylamba.com/s7/fleet/*
 // @match        http://game.asylamba.com/s8/fleet/*
 // @match        http://game.asylamba.com/s9/fleet/*
+// @match        http://game.asylamba.com/s10/fleet/*
 // @grant        none
 // ==/UserScript==
 
